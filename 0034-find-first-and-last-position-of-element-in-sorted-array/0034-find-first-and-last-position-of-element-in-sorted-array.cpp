2834663cdde4d051e39e80a35c7e4d13b62a13e8
Sahil arora <100480759+sahilarora02@users.noreply.github.com>
2022-11-12 19:39:19 +0530

    Time: 13 ms (71.58%), Space: 13.7 MB (18.75%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-          int n = nums.size();
-        vector<int> ans ;
-        int start = -1 , end = -1;
-        int  i = 0;
-        
-        for(int k = 0 ; k < n ; k ++){
-            if(nums[k] != target){
-               continue;
-            }else{
-                start = k;
-                break;
-            }
+        int n = nums.size();
+        vector<int> v;
+        int count  = 0 ;
+        for(int i =  0; i<n ;i++){
+        if(nums[i]==target){
+              count++;
+         }
         }
-        
-          for(int k = n-1 ; k >=0 ; k --){
-            if(nums[k] != target){
-               continue;
-            }else{
-                end = k;
-                break;
-            }
+         if(count == 0){
+        v.push_back(-1);
+        v.push_back(-1);
+        return v;
         }
-        
-
-        
-        ans.push_back(start);
-        ans.push_back(end);
-        
-        return ans;
+        int c2 = 1;
+        for(int i = 0 ; i< n ; i++){
+           if(nums[i] == target && (c2 == 1 || c2 == count)  ){
+           v.push_back(i);
+           c2++;
+           }else if(target == nums[i]){
+               c2++;
+           }
+        }
+        if(v.size()==1){
+            int front  = v.front();
+            v.push_back(front);
+        }
+        return v;
        
     }
 };
