4c11cd95a6581763f00f3a9b773bbf8bbf85b7a6
Sahil arora <100480759+sahilarora02@users.noreply.github.com>
2022-11-12 08:18:50 +0530

    Time: 9 ms (51.25%), Space: 8.8 MB (55.84%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,33 +2,45 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int maxInd = 0 ,  max  = nums[0];
-        int n = nums.size();
-        if(n == 1){
+       int n = nums.size();
+        int max = INT_MIN;
+        int ans ;
+        
+        if(n==1){
             return 0;
         }
-        // unordered_map<int , pair<int , int> >
-        for(int i  = 0  ; i <n ; i++){
+        
+        for(int i = 0; i <n ; i++){
+                            if(i==0){
+                                if(nums[i]>nums[i+1]){
+                                  
+                                    if(nums[i]>max)
+                                      {  max = nums[i]; 
+                                    ans = i ;}
+                                }
+                            }
+         
             
-             if(i == 0  ){
-                if(nums[i] > nums[i+1]  && nums[i]>max){
-                    max = nums[i];
-                    maxInd = i;
-                }
-            }
-            else if(i == n-1){
-                if(nums[i] > nums[i-1] && nums[i]>max){
-                    max = nums[i];
-                    maxInd = i;
-                }
-            }
-            else{
-                if(nums[i] > nums[i+1] && nums[i] > nums[i-1] && nums[i]>max){
-                    max = nums[i];
-                    maxInd = i;
-                }
-            }
+                            else if(i==n-1){
+                                  if(nums[i]>nums[i-1]){
+                                      
+                                    if(nums[i]>max)
+                                       { max = nums[i]; 
+                                      ans = i ;}
+                                }
+                            }
+                            else{
+                                   if(nums[i]>nums[i+1] && nums[i]>nums[i-1]){
+                                      if(nums[i]>max)
+                                        {  max = nums[i];
+                                       ans = i ;}
+                                     }
+
+                            }
+    
+         
         }
-        return maxInd;
+        
+        return ans; 
     }
 };
